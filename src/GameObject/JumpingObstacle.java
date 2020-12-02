1493608e27733585075c38e98e86b7d51376364a
lamnguyen8075 <48584294+lamnguyen8075@users.noreply.github.com>
2020-12-02 01:44:30 -0800

    Add files via upload
    JumpingObstacle is appeared on the panel but It doesn't behave correctly.

--- src/GameObject/JumpingObstacle.java
+++ src/GameObject/JumpingObstacle.java
@@ -1,4 +1,5 @@
 package GameObject;
+import java.awt.Color;
 import java.awt.Graphics;
 import Systems.GameManager;
 
@@ -11,62 +12,54 @@ import Systems.GameManager;
  */
 public class JumpingObstacle extends Obstacle {
 
-	private static final float JUMPFORCE = 1650; 					//yVelocity initial speed upon jump
+	private static final float JUMPFORCE = 1000; 					//yVelocity initial speed upon jump
 	private static final float GRAVITY = -6000; 
-	public static final int WIDTH = 40, HEIGHT = 75;
-	private static float x = 50, y = GameManager.GROUND_POSITION; 
+	public static final int WIDTH = 60, HEIGHT = 85;
+	private static float x = 5, y = GameManager.GROUND_POSITION; 
 	private float yVel = 0; 										//player's Y speed	
 	
-	//0, 0 is top left corner
-	//GameManager.PIXEL_WIDTH, GameManager.PIXEL_HEIGHT is bottom right corner
-	public static float getX() { 
-		return x; 
-	}
-
-	public static float getY() { 
-		return y; 
-	}
 
 	public JumpingObstacle(int width, int height) {
 		super(width, height);
-	}
-
-	// TODO implement the move method
-	@Override
-	void move() {	
-		x -= GameManager.getScrollSpeed() * GameManager.getDeltaTime();
-	}
-
-	// TODO implement the display method
-	@Override
-	public void display(Graphics g) {
-
 	}
 	
 	//TODO implement update to work with the jump method
 	@Override
 	public void update() {
 		move();
-		jump();
-		checkPlayerCollision();
-		checkExitScreen();
+		gravity();
+	}
+	
+	// TODO implement the move method
+	@Override
+	void move() {
+		x += (GameManager.getScrollSpeed() * GameManager.getDeltaTime())/8;
+		
 	}
 	
 	//gravity constantly increments y coordinate based on yVelocity
 	void gravity() {
 		y -= yVel * GameManager.getDeltaTime(); 
-		
+		yVel = 0;
 		if (y < GameManager.GROUND_POSITION - HEIGHT) //airborne decrement yVelocity
 			yVel += GRAVITY * GameManager.getDeltaTime();
 		else //grounded, set yVelocity to 0
 		{
-			yVel = 0;  //obstacle is grounded, no y velocity
-			y = GameManager.GROUND_POSITION - HEIGHT;
+			//jump when obstacle is grounded
+			yVel = JUMPFORCE;
 		}
 	}
 
 	void jump() {
 		if (y >= GameManager.GROUND_POSITION - HEIGHT) //obstacle is grounded 
 			yVel = JUMPFORCE;  //Instantaneously set Y velocity in jump
+	}
+	
+	// TODO implement the display method 
+	@Override
+	public void display(Graphics g) {
+		g.drawRect((int)x, (int)y, WIDTH, HEIGHT);
+		g.setColor(Color.green);
+		g.fillRect((int)x, (int)y, WIDTH, HEIGHT);
 	}
 }
