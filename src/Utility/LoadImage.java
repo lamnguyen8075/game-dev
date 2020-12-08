f0e755039133b2e11cc2b56deeadd5562fd3f4d9
phuong-tong <71061461+phuong-tong@users.noreply.github.com>
2020-12-08 00:30:28 -0800

    Add files via upload
    add java doc

--- src/Utility/LoadImage.java
+++ src/Utility/LoadImage.java
@@ -4,8 +4,20 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-
+/**
+ * The LoadImage program is used to handle and operate directly 
+ * with image data. 
+ *
+ */
 public class LoadImage {
+	/**
+	 * This is the method is used to get the image from file
+	 * with a provided path.
+	 * 
+	 * @param path a String parameter providing the path of the image
+	 * @return the desired image from the file
+	 * @exception IOException on input error
+	 */
 	public static BufferedImage loadImage(String path) {
 		BufferedImage img = null;
 		try {
--- src/Utility/UniqueIDGenerator.java
+++ src/Utility/UniqueIDGenerator.java
@@ -1,10 +1,17 @@
 package Utility;
-
+/**
+ * The program is used for creating the unique ID for 
+ * each GameObject.
+ */
 public class UniqueIDGenerator {
 	
 	private static int id = 0;
 	
 	//WIP
+	/** Method to get the unique ID for the object
+	 * 
+	 * @return the ID of the object
+	 */
 	public static int getID() {
 		return id++;
 	}
