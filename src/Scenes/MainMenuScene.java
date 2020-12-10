6e8e05f61ba5884220770d660aee8386978db501
797bao <797nguyenbao@gmail.com>
2020-12-09 17:41:57 -0800

    Add files via upload
    added docs for MainScene

--- src/Scenes/MainMenuScene.java
+++ src/Scenes/MainMenuScene.java
@@ -2,9 +2,11 @@ package Scenes;
 import GameObject.ScoreText;
 import GameObject.StartButton;
 
-//TODO implement the panel's paint behaviour 
-//TODO implement the start button
-//TODO implement the quit button
+/**
+ * The Main Menu scene where the player can restart the game
+ * contains only a text for the score and a start button
+ * Essentially a class purely used to contain the data of that scene
+ */
 public class MainMenuScene{
 	
 	public MainMenuScene()
