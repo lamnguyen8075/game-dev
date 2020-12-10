433e9ae8bd9d8b6fc7a050679fba4cfc53963519
797bao <797nguyenbao@gmail.com>
2020-12-09 17:41:29 -0800

    Update GameScene.java

--- src/Scenes/GameScene.java
+++ src/Scenes/GameScene.java
@@ -4,6 +4,11 @@ import GameObject.Player;
 import GameObject.ScoreText;
 import GameObject.SpawnManager;
 
+/**
+ * The Game scene where the player can run & jump past obstacles
+ * contains all the initial objects within the game scene
+ * Essentially a class purely used to contain the data of that scene
+ */
 public class GameScene  {
 	
 	public GameScene()
